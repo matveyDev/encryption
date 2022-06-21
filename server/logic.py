8d671050397dd036320964a877b57760922cbee9
Matvey Semenov <matveytri777@gmail.com>
2022-06-21 18:48:56 +0300

    completed upload_decrypted_data

--- server/logic.py
+++ server/logic.py
@@ -1,11 +1,13 @@
-import requests
 import json
 
-from sqlalchemy import create_engine
+from base64 import b64encode
+import requests
+
+from sqlalchemy import create_engine, update, select
 from sqlalchemy.orm import Session
 
-from core.models import Encryption
 from core.database.db import SQLALCHEMY_DATABASE_URL
+from core.models import Encryption
 from settings import API_URL
 
 
@@ -13,9 +15,24 @@ class EncryptorAPI:
 
     def __init__(self) -> None:
         self.url_encrypted_data = API_URL + 'top-secret-data'
+        self.url_decrypt_data = API_URL + 'decrypt'
 
     async def _get_encrypted_data(self) -> list[str]:
-        response = requests.get(self.url_encrypted_data)
+        response = requests.get(url=self.url_encrypted_data)
+        encrypted_data = json.loads(response.text)
+        return encrypted_data
+
+    async def _get_decrypted_data(self, encrypted_data: list[str]) -> list[str]:
+        json_data = json.dumps(encrypted_data)
+        auth = b64encode(b'qummy:GiVEmYsecReT!').decode('ascii')
+        headers = {
+            'Authorization': f'Basic {auth}'
+        }
+        response = requests.post(
+            url=self.url_decrypt_data,
+            data=json_data,
+            headers=headers
+        )
         encrypted_data = json.loads(response.text)
         return encrypted_data
 
@@ -26,12 +43,31 @@ class EncryptorUploader:
         engine = create_engine(SQLALCHEMY_DATABASE_URL)
         self.session = Session(engine)
 
-    def upload_database(self, data: list[str]) -> None:
+    def upload_encrypted_data(self, data: list[str]) -> None:
         objects_to_create = []
-        for text in range(len(data)):
+        for text in data:
             objects_to_create.append(
                 Encryption(encrypted_text=text)
             )
 
         self.session.bulk_save_objects(objects_to_create)
         self.session.commit()
+
+    def upload_decrypted_data(
+            self,
+            encrypted_data: list[str],
+            decrypted_data: list[str]
+        ) -> None:
+        objects_to_update = []
+        for en_string, de_string in zip(encrypted_data, decrypted_data):
+            objects_to_update.append({
+                'encrypted_text': en_string,
+                'decrypted_text': de_string,
+            })
+            query = (
+                update(Encryption).
+                values(decrypted_text = de_string).
+                where(Encryption.encrypted_text == en_string)
+            )
+            self.session.execute(query)
+            self.session.commit()
