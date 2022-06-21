cc3fc2960c0491d4c4ddf9a3389f79794e5adb9b
Matvey Semenov <matveytri777@gmail.com>
2022-06-21 19:50:25 +0300

    added send_result endpoint

--- server/api.py
+++ server/api.py
@@ -1,24 +1,31 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 
-from logic import EncryptorUploader, EncryptorAPI
+from logic import EncryptorBase
+from schemas import DecryptedList, EncryptedList
 
 router = APIRouter()
-uploader = EncryptorUploader()
-API = EncryptorAPI()
+encryptor = EncryptorBase()
 
 
-@router.get('/upload-encrypted-data', status_code=201)
+@router.post('/upload-encrypted-data', status_code=201, response_model=list[EncryptedList])
 async def upload_encrypted_data() -> list[str]:
-    data = await API._get_encrypted_data()
-    uploader.upload_encrypted_data(data)
+    data = await encryptor._get_encrypted_data()
+    encryptor.upload_encrypted_data(data)
 
     return data
 
 
-@router.post('/upload-decrypted-data', status_code=201)
+@router.post('/upload-decrypted-data', status_code=201, response_model=list[DecryptedList])
 async def upload_decrypted_data() -> list[str]:
-    encrypted_data = await API._get_encrypted_data()
-    decrypted_data = await API._get_decrypted_data(encrypted_data)
-    uploader.upload_decrypted_data(encrypted_data, decrypted_data)
+    encrypted_data = await encryptor._get_encrypted_data()
+    decrypted_data = await encryptor._get_decrypted_data(encrypted_data)
+    encryptor.upload_decrypted_data(encrypted_data, decrypted_data)
 
     return decrypted_data
+
+
+@router.post('/send-result', status_code=202)
+async def send_result() -> None:
+    encryptor.send_result()
+
+    return Response(status_code=202)
