317a3eb86ca42aded18d36d39503c4ba180ef605
Matvey Semenov <matveytri777@gmail.com>
2022-06-21 19:49:51 +0300

    added send result method

--- server/logic.py
+++ server/logic.py
@@ -1,14 +1,13 @@
 import json
-
 from base64 import b64encode
-import requests
 
-from sqlalchemy import create_engine, update, select
+import requests
+from sqlalchemy import create_engine, update
 from sqlalchemy.orm import Session
 
 from core.database.db import SQLALCHEMY_DATABASE_URL
 from core.models import Encryption
-from settings import API_URL
+from settings import API_URL, RESULT
 
 
 class EncryptorAPI:
@@ -16,6 +15,7 @@ class EncryptorAPI:
     def __init__(self) -> None:
         self.url_encrypted_data = API_URL + 'top-secret-data'
         self.url_decrypt_data = API_URL + 'decrypt'
+        self.url_result = API_URL + 'result'
 
     async def _get_encrypted_data(self) -> list[str]:
         response = requests.get(url=self.url_encrypted_data)
@@ -37,9 +37,10 @@ class EncryptorAPI:
         return encrypted_data
 
 
-class EncryptorUploader:
+class EncryptorBase(EncryptorAPI):
 
     def __init__(self) -> None:
+        super().__init__()
         engine = create_engine(SQLALCHEMY_DATABASE_URL)
         self.session = Session(engine)
 
@@ -71,3 +72,15 @@ class EncryptorUploader:
             )
             self.session.execute(query)
             self.session.commit()
+
+    def send_result(self) -> None:
+        decrypted_texts = self.session.query(Encryption.decrypted_text)
+        values = list(decrypted_texts)
+
+        result = []
+        for idx in range(len(values)):
+            result.append(values[idx][0])
+            
+        RESULT['result'] = result
+        data = json.dumps(RESULT)
+        # _ = requests.post(url=self.url_result, data=data)
